bf1689ec9cdf2f554e4b5240dbd813e30e41ea37
bshaw1234 <bshaw2623@gmail.com>
2023-10-02 00:57:36 +0530

    context-api quiz questions

--- src/Components/Context/Provider.js
+++ src/Components/Context/Provider.js
@@ -3,12 +3,22 @@ const ChatContext = createContext();
 
 const Provider = ({ children }) => {
   const [activeTab, setActiveTab] = useState('tab1');
-
+  const [quest, setquest] = useState({
+    Subject: "",
+    Class: "",
+    Topic: "",
+    Sub_topic: "",
+    Level: "",
+    Quiz_Type: "",
+    Language:"English",
+  });
   return (
     <ChatContext.Provider
       value={{
               activeTab,
-              setActiveTab
+              setActiveTab,
+              quest,
+              setquest
       }}
     >
       {children}
