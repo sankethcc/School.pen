891e6674a8bcd853caa0b13d2667b7b6b1acd799
sankethcc <sankethcc@gmail.com>
2023-10-03 23:26:44 +0530

    Add subject Preview Question

--- src/Components/QuizP1/Preview/SideDetails.jsx
+++ src/Components/QuizP1/Preview/SideDetails.jsx
@@ -1,38 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import edits  from '../assets/edit.png'
-
+import axios from 'axios'
+import { Box } from "@mui/material"
+import { State } from '../../Context/Provider'
 const SideDetails = ({heading, number}) => {
+  const { questions} = State();
+
   return (
+    
     <div className="side-details">
       <div className="amount-wrapper">
         <p className="price-heading">{heading}</p>
-        <p className="price-count">{number}</p>
+        <p className="price-count">{questions.length}</p>
       </div>
       <h3>Preview</h3>
-      <div className='preview-question'>
-        <img src={edits} className='edit-logo' alt="" />
-        <p>Tadoba national park known for sheltering tiger, panther and bear is located in: </p>
-        <ul>
-          <li>Assam1</li>
-          <li>Chandrapur</li>
-          <li>Karnataka</li>
-          <li>Tamil Nadu</li>
-        </ul>
-      </div>
-      <div className='preview-question'>
-        <img src={edits} className='edit-logo' alt="" />
-        <div>
-          <img className='' src="" alt="" />
-          <p>Tadoba national park known for sheltering tiger, panther and bear is located in: </p>
+      <Box>
+       {questions?.map((data, i)=>{
+        const {question, options } = data.question_container
+        return(
+          <div className='preview-question' key={i}>
+          <img src={edits} className='edit-logo' alt="" />
+          <p>{question}</p>
           <ul>
-            <li>Assam</li>
-            <li>Chandrapur</li>
-            <li>Karnataka</li>
-            <li>Tamil Nadu</li>
+            {options.map((option, i)=>{
+              const{text, image_url} = option
+              return (
+                <li key={i}>{text}</li>
+              )
+            })}
+            
           </ul>
         </div>
-        
-      </div>
+        )
+        })}
+
+      </Box>
+
     </div>
   )
 }
