976ddc1e70dd0c56b29953172d21fb231af434ce
bshaw1234 <bshaw2623@gmail.com>
2023-10-05 01:39:05 +0530

    real time review

--- src/Components/AllComponents/SelectContainer.jsx
+++ src/Components/AllComponents/SelectContainer.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import UnstyledSelectObjectValues from '../CreateQuiz/UnstyledSelectObjectValues'
 import { Box } from '@mui/system'
+import { State } from '../Context/Provider'
+
 
 const SelectContainer = () => {
+  const { dsubject,dtopic,dstopic} = State();
   return (
     <Box 
     sx={{
@@ -15,9 +18,9 @@ const SelectContainer = () => {
             borderRadius:'40px'
         }}>
         <UnstyledSelectObjectValues dropdownName={"Class"} listArray={["1", "2", "3", "4", "5", "6", "7", "8", "9"]} classList={"classChange"} add={true} value={"Class"} />
-        <UnstyledSelectObjectValues dropdownName={"Subject"} listArray={["Science", "Mathematics", "History"]} add={true} value={"Subject"}/>
-        <UnstyledSelectObjectValues dropdownName={"Topic"} listArray={["Biology", "Chemistry", "Physics"] } add={true} value={"Topic"}/>
-        <UnstyledSelectObjectValues dropdownName={"Sub topic"} listArray={["Genetics", "Ecology", "Human anatomy"]}add={true} value={"Sub_topic"}/>
+        <UnstyledSelectObjectValues dropdownName={"Subject"} listArray={dsubject} add={true} value={"Subject"}/>
+        <UnstyledSelectObjectValues dropdownName={"Topic"} listArray={dtopic} add={true} value={"Topic"}/>
+        <UnstyledSelectObjectValues dropdownName={"Sub topic"} listArray={dstopic}add={true} value={"Sub_topic"}/>
         <UnstyledSelectObjectValues dropdownName={"Level"} listArray={["Beginner", "Intermediate" , "Advance"]} add={false} value={"Level"}/>
         <UnstyledSelectObjectValues dropdownName={"Quiz Type"} listArray={["Multiple choice - Single answer", "Multiple choice - multiple answers", "Yes or No", "True or False"]} add={false} value={"Quiz_Type"}/>
 
